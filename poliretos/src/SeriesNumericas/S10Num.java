f2fd7805d5371ed693adee54e1e5f3a6647c898e
Eduardo <169008489+MatiuC@users.noreply.github.com>
2024-11-01 16:16:00 -0500

    Serie 10 y Serie 12

---
+++ poliretos/src/SeriesNumericas/S10Num.java
@@ -0,0 +1,9 @@
+package SeriesNumericas;
+
+public class S10Num {
+    public void SerieNum10(int n){
+        for (int i = 1; i < n; i++) {
+            System.out.print((int)Math.pow(3, i) + ", ");
+        }
+    }
+}
