8557785ca99ad86e6c112589c69e30879f27b05f
Eduardo <169008489+MatiuC@users.noreply.github.com>
2024-11-10 22:40:42 -0500

    Actualizar ruta del archivo de datos en App.java y mejorar formato de código en Automata03.java; se agregan nuevas cadenas de prueba en data.txt.

--- poliretos/src/App.java
+++ poliretos/src/App.java
@@ -1,8 +1,8 @@
-import GrafosAutomatas.Automata03;
+import GrafosAutomatas.*;
 
 public class App {
     public static void main(String[] args) {
-        String filename ="src/assets/data.txt";
+        String filename ="poliretos/src/assets/data.txt";
         Automata03 automata = new Automata03();
         automata.processFile(filename);
     }
