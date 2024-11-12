6aa93a590f160453598b725020fe0d1437092f28
Bryan Ayala <bryan.ayala02@epn.edu.ec>
2024-11-11 23:50:36 -0500

    Automata A06 Completo

--- poliretos/src/App.java
+++ poliretos/src/App.java
@@ -4,59 +4,9 @@ import CadenaCaracteres.*;
 import Figuras.*;
 import Arrays.*;
 import Loading.*;
+import GrafosAutomatas.*;
 public class App {
     public static void main(String[] args) throws Exception {
-        // System.out.println("Hello, World!");
-        // S10Num S10= new S10Num();
-        // S12Num S12 = new S12Num();
-        // S12.SerieNum12(10);
-        // System.out.println("");
-        // S10.SerieNum10(10);
-        
-        // S7Num S7 = new S7Num();
-        // S7.g1_mostrarSecuencia(20);
-        // S8Num S8 = new S8Num();
-        // S8.g1_mostrarSecuencia(20);
-        // S9Num S9 = new S9Num();
-        // S9.g1_mostrarSecuencia(20);
-
-        // S7Char S7 = new S7Char();
-        // S7.g1_mostrarSecuenciaChar(6);
-        
-        // S8Char S8 = new S8Char();
-        // S8.g1_mostrarSecuenciaChar(6);
-
-        // S9Char S9 = new S9Char();
-        // S9.g1_mostrarSecuenciaChar(10);
-
-        // F12 F12 = new F12();
-        // F12.g1_mostrarFigura(10);
-
-        // F13 F13 = new F13();
-        // F13.g1_mostrarFigura(10);
-
-        // F17 F17 = new F17();
-        // F17.g1_mostrarFigura(10);
-
-        // C03 C03 = new C03();
-        // C03.g1_eliminarVocal();
-
-        // C07 C07 = new C07();
-        // C07.g1_convertirSinJ();
-
-
-        // A02 A02 = new A02();
-        // A02.g1_matrizIniciales(5);
-
-        L02 L02 = new L02();
-        L02.g1_iniciar();
-
-        
-        L06 L06 = new L06();
-        L06.g1_iniciar();
-
-        L10 L10 = new L10();
-        L10.g1_iniciar();
 
     }
 }
