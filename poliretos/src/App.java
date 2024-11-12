b6e001f72b6b99ad35ee19ed68caa42229b1b825
Eduardo <169008489+MatiuC@users.noreply.github.com>
2024-11-12 00:05:24 -0500

    Agregar mensaje de agradecimiento al final de la ejecución en App.java

--- poliretos/src/App.java
+++ poliretos/src/App.java
@@ -3,5 +3,6 @@ public class App {
     public static void main(String[] args) {
         menu m = new menu();    
         m.mostrar_menu();
+        System.out.println("Gracias por usar el programa");
     }
 }
