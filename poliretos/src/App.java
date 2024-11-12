9ffcec3e664a7ea25ba7de5bda5f9fde1c6dd52a
Bryan Ayala <bryan.ayala02@epn.edu.ec>
2024-11-12 00:17:07 -0500

    Automata A07 Completo

--- poliretos/src/App.java
+++ poliretos/src/App.java
@@ -8,5 +8,7 @@ import GrafosAutomatas.*;
 public class App {
     public static void main(String[] args) throws Exception {
 
+        Automata07 au = new Automata07();
+        au.validarEntrada();
     }
 }
